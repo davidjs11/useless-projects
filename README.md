aef63aefa7f008e60a7f7052f5666cb5eb03d076
davidjs11 <davidjandulasanchez11@gmail.com>
2023-04-21 14:38:13 +0200

    added the dvd bouncing logo project

--- README.md
+++ README.md
@@ -4,16 +4,28 @@ just pointless projects. nothing else. don't expect a complex app or a highly ef
 - the reason i write this README in lower case is because, for me, it takes away from the seriousness of the repository. obviously most of these projects are mere pastimes that come out of laughts.
 - i repeat, don't take this as a professional repository. the code here can be made much better, but as the title says, they're useless and nonsense projects.
 - if you feel like trying a project, i hope you have fun! and don't forget giving any feedback if you consider necessary :)
+- don't take the "possible features" seriously. it's very probable that they never come.
 
 # projects made so far:
-## arduino breathalyzer (neutral evil)
+## arduino breathalyzer
 just an arduino project that measures the alcohol level in a gas. this project was made using the mq3 sensor, and showing the output through the usb serial port. it is currently in development (but i don't have the necessary components). i haven't tested it yet while drinking, but it will happen someday.
 ##### how to use the arduino scripts
 - the `load.sh` script loads a project onto the arduino board. you must insert the .ino file as an argument.
 - the `serial.sh` script outputs the serial signal on the terminal. you must insert the correct baudrate (I'll use 9600 in every arduino project).
+##### possible future features
+- improve the measuring system
+- lcd screen showing the alcohol level
 
-## bouncing dvd logo (don't uploaded yet)
+## bouncing dvd logo
 the idea for this project came up during a really boring class. it was developed in less than 30 minutes, so the code is obviously very improvable. it uses sdl2 as graphic library.
+#### how to compile
+- just write `make` on the project folder and done! 
+- if you stare at the screen hoping that it touches the corner, reconsider the way you employ your time (ok i'm not gonna lie i did it once and it touched the corner...)
+#### possible future features
+- collision counter
+- changing color
+- random direction at the beginning
+- speed changing
 
 ## pi generator (not even started)
 ok this is actually epic. it generates pi using random numbers. but i haven't made it yet. but i'll make it. i promise.
---
+++ dvd/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+// struct for the logo information
+struct logo
+{
+    SDL_Rect rect;
+    SDL_Surface *dvdLogo;
+    SDL_Texture *texture;
+    float posX, posY;
+    float speedX, speedY;
+};
+
+// functions
+void renderLogo(SDL_Renderer *rend, struct logo *dvd);
+void moveLogo(struct logo *dvd);
+int checkHorizontalCollision(struct logo *dvd);
+int checkVerticalCollision(struct logo *dvd);
+
+int main(void)
+{
+    // SDL stuff
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *win = SDL_CreateWindow("DVD", SDL_WINDOWPOS_UNDEFINED,
+                                       SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
+    SDL_Renderer *rend = SDL_CreateRenderer(win, SDL_RENDERER_ACCELERATED, 0);
+    SDL_Event event;
+    int running = 1;
+
+    // logo initialization
+    struct logo dvd;
+    dvd.rect.w = 256;
+    dvd.rect.h = 130;
+    dvd.posX = SCREEN_WIDTH/2;
+    dvd.posY = SCREEN_HEIGHT/2;
+    dvd.speedX = -4.75f;
+    dvd.speedY = -2.0f;
+    dvd.dvdLogo = IMG_Load("logo.png");
+    dvd.texture = SDL_CreateTextureFromSurface(rend, dvd.dvdLogo);
+
+    // game loop
+    while(running)
+    {
+        // event processing
+        while(SDL_PollEvent(&event))
+            running = (event.type != SDL_QUIT);
+        
+        // processing
+        moveLogo(&dvd);
+
+        // rendering
+        SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(rend);
+        renderLogo(rend, &dvd);
+        SDL_RenderPresent(rend);
+
+        SDL_Delay(20);
+    }
+}
+
+// wow a one line function!!! (nah soon it will have more lines)
+void renderLogo(SDL_Renderer *rend, struct logo *dvd)
+{
+    SDL_RenderCopy(rend, dvd->texture, NULL, &(dvd->rect));
+}
+
+void moveLogo(struct logo *dvd)
+{
+    // update logo position
+    dvd->posX += dvd->speedX;
+    dvd->posY += dvd->speedY;
+
+    // collision detection
+    dvd->speedX *= (checkHorizontalCollision(dvd)) ? -1 : 1;
+    dvd->speedY *= (checkVerticalCollision(dvd)) ? -1 : 1;
+
+    // update rect position
+    dvd->rect.x = dvd->posX;
+    dvd->rect.y = dvd->posY;
+}
+
+int checkHorizontalCollision(struct logo *dvd)
+{
+    return ((dvd->posX + dvd->rect.w) >= SCREEN_WIDTH || dvd->posX <= 0);
+}
+
+int checkVerticalCollision(struct logo *dvd)
+{
+    return ((dvd->posY + dvd->rect.h) >= SCREEN_HEIGHT || dvd->posY <= 0);
+}
